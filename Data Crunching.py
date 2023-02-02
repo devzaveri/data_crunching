5f5c0373afa93989a2d57d578308ea8946015129
Dev Zaveri <devsoni5880@gmail.com>
2023-02-02 13:19:51 +0530

    some change in code

--- Data Crunching.py
+++ Data Crunching.py
@@ -1,10 +1,11 @@
 import pandas as pd
 
-file_list = ['user_email_hash.1m.tsv',  'plain_32m.tsv' , 'ip_1m.tsv']
+FL = ['user_email_hash.1m.tsv',  'plain_32m.tsv' , 'ip_1m.tsv']
 
-df_list = [pd.read_csv(f, sep='\t', header=None) for f in file_list]
-df = pd.concat(df_list, axis=1, ignore_index=True)
+readD = [pd.read_csv(f, sep='\t', header=None) for f in FL]
+df = pd.concat(readD, axis=1, ignore_index=True)
 
 col_names = ['Id', 'username', 'email', 'hashed_password', 'plaintext_password', 'ip']
 
 df.to_csv("merged_file.tsv", sep='\t', index=False, header=True)
+
